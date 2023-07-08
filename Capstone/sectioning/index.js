8357b6c8de8ba08472e7e44f0907d49035405d78
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-07-08 12:56:31 -0500

    add sectioning and view folder (#9)

---
+++ Capstone/sectioning/index.js
@@ -0,0 +1,8 @@
+import AboutUs from "./About-us";
+import Contact from "./Contact";
+import GiftCards from "./Gift-cards";
+import Landing from "./Landing";
+import Menu from "./Menu";
+import OrderPay from "./Order-pay";
+
+export { AboutUs, Contact, GiftCards, Landing, Menu, OrderPay};

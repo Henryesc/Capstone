345ed506b414c640a6f90432a40be3106fe87b8e
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-07-15 13:54:38 -0400

    add testimonials further details

--- Capstone/view/Landing.jsx
+++ Capstone/view/Landing.jsx
@@ -1,10 +1,19 @@
+import React from 'react'
+import ImageCarousel from '../components/Testimonials'
+import CateringRequest from '../components/CateringRequest'
 import { Background } from "../components"
 
+
 const Landing = () => {
+
   return (
-    <>
+    <div>
       <Background />
-    </>
-  )
-}
+    <ImageCarousel />
+    <CateringRequest />
+  </div>
+);
+};
+
+
 export default Landing

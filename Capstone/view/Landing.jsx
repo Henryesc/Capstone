bc6e02f0cd07b5f97e9bace1e82fa551af11d68f
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-07-15 16:45:09 -0400

    start cateringRequest component

--- Capstone/view/Landing.jsx
+++ Capstone/view/Landing.jsx
@@ -1,11 +1,13 @@
 import { AboutUs, Background } from "../components"
+import CateringRequest from "../components/CateringRequest"
 
 const Landing = () => {
   return (
     <>
       <Background />
       <AboutUs />
+      <CateringRequest />
     </>
-  )
+  );
 }
 export default Landing

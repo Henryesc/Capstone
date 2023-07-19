49ceacc5594f93393be6dd28bf2a91f3f7c50bdf
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-07-18 22:24:49 -0400

    add styles code for catering section component

--- Capstone/components/CateringRequest.jsx
+++ Capstone/components/CateringRequest.jsx
@@ -6,14 +6,24 @@ const CateringRequest = () => {
 
   return (
     <div className="cateringContainer">
-    <div className="cateringRequest">
-        <h1 className="cateringRequestTitle">Catering Request</h1>
-        <span className="cateringRequestText">Please give us at least 10 days before the desired Catering Date</span>
+      <div className="cateringRequestTextContainer">
+        <h1 className="cateringRequestTitle">Catering Request </h1>
+      </div>
+      <br/>
+      <div className="cateringRequest">
+        <h1 className="catering">
+          DO YOU <span className="catering2"> GOVINDA'S VEGETARIAN </span>
+          CUISINE?
+        </h1>
+        <br /> <br />
+        <span className="cateringRequestText">
+          Please give us at least 10 days before the desired Catering Date
+        </span>
       </div>
       <div className="cateringButtonContainer">
         <button className="cateringButton">Request Catering</button>
-        </div>
       </div>
+    </div>
   );
   
 };

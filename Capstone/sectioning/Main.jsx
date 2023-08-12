3ec09641af6376c1cff17a29e0f96a20a387e500
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-08-12 03:55:14 +0200

    085/request catering (#91)
    * styles (catering) regular styles removed
    
    * fix (Landing) global export and import
    
    * fix (Landing) add styled components and mobile responsive
    
    * feat (nav) route and view added

--- Capstone/sectioning/Main.jsx
+++ Capstone/sectioning/Main.jsx
@@ -1,6 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { Landing, AboutUs, Contact, GiftCards, Menu, OrderPay, Error, Success } from "../view";
-
+import {
+  Landing,
+  AboutUs,
+  Contact,
+  GiftCards,
+  Menu,
+  OrderPay,
+  Error,
+  Success,
+  Catering,
+} from "../view";
 
 const Main = () => {
   return (
@@ -13,9 +22,9 @@ const Main = () => {
         <Route path="menus" element={<Menu />} />
         <Route path="order-pay" element={<OrderPay />} />
         <Route path="order/success" element={<Success />} />
+        <Route path="catering" element={<Catering />} />
         <Route path="*" element={<Error />} />
       </Routes>
-     
     </main>
   );
 };

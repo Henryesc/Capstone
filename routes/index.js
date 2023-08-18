f8a3adfb309aa76f14a7b684ff801db668a32476
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-08-18 03:57:20 +0200

    feat (index) export the route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const giftCardsRouter = require("./gift_cards");
 const sessionIdRetrieves = require("./session_id_retrieves");
+const uploads = require("./upload_images");
 
-module.exports = { giftCardsRouter, sessionIdRetrieves };
+module.exports = { giftCardsRouter, sessionIdRetrieves, uploads };

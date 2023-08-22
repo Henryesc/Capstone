4aa4370447d1ffb0b4bf1f77cdeba35c948480db
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-08-22 04:43:31 +0200

    fix (server file) moved endpoints to it self file in routes

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const server = express();
-const pgp = require("pg-promise")();
-const db = pgp(process.env.URL);
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3030;
 
-const {giftCardsRouter, sessionIdRetrieves} = require("./routes");
+const {giftCardsRouter, sessionIdRetrieves, uploads, insertReview} = require("./routes");
 
 
 
@@ -24,23 +21,6 @@ server.get('/heartbeat', (req, res) => {
     res.json({
         "message": "Heartbeat"
     });
-});
-
-server.post("/add-review", async (req, res) => {
-    const { name, rating, comment } = req.body;
-
-    try {
-        await db.none(`
-            INSERT INTO reviews (name, rating, comment)
-            VALUES ($1, $2, $3)
-        `, [name, rating, comment]);
-
-        res.status(200).json({message: "Review added successfully"});
-        
-    } catch (err) {
-        console.error("Error,", err);
-        res.status(500).json({error: "Something went wrong"});
-    }
 });
 
 server.post("/gift-card", async (req, res) => {
@@ -63,8 +43,10 @@ server.post("/gift-card", async (req, res) => {
 
 server.use("/", giftCardsRouter);
 server.use("/", sessionIdRetrieves);
+server.use("/", uploads);
+server.use("/", insertReview);
+
 
 server.listen(PORT, () => {
     console.log(`The server is running at PORT ${PORT}`);
 });
-
